b81bf5b766bfee2288a4825cb3b770beeb7ebf74
nerdreich <git@nerdreich.at>
2020-08-24 00:50:17 +0200

    added burger icon

--- src/theme/setup.php
+++ src/theme/setup.php
@@ -128,7 +128,7 @@ function outputNavbar(at\nerdreich\Wiki $wiki, at\nerdreich\UserSession $user)
         <?php echo $wiki->getSnippetHTML('topnav'); ?>
         <div>
           <input id="wiki-burger" type="checkbox">
-          <label for="wiki-burger">[=]</label>
+          <label for="wiki-burger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></label>
           <div class="wiki-menu">
             <?php echo getPageLinksHTML($user, $wiki); ?>
           </div>
